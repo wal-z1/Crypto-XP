7870aaf5d2d7adc6ee7c593be0dd898315c22fa6
Walid <ow_bouhenika@esi.dz>
2026-06-28 13:58:39 +0100

    idk 27 mins of the vid already

--- test1.py
+++ test1.py
@@ -1,3 +1,6 @@
+
+
+
 walid = 1234
 walid_string = "walid"
 
@@ -15,4 +18,30 @@ print(walid,walid_string,walid_list,walid_boolTrue,walid_boolFalse)
 ending= '\n'
 
 print("walid was here or something idk",end=ending)
+
+lyrics = """
+i wanna grab by shoulders
+and shake baby
+arctic monkeys - snap out of it
+
+"""
+
+import math
+
+expension = math.exp(1)
+
+print(expension)
+print(expension**3)
+print(expension // 2)
+print("mod",expension % 2)
+
+## every read number is a float by default
+walid_read = input("Enter a number: ")
+print(type(walid_read))
+
+print(lyrics.capitalize(),lyrics.upper(),lyrics.lower())
+print(lyrics.count("l"))
+
+if(math.exp(1) % 3 != math.exp(1) // 2 ):
+    print("exp(1) % 3 is equal to exp(1) // 2",math.exp(1)%3,math.exp(1) // 2)
 
