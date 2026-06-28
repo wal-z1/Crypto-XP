06f020ea0175743f50677d0fe56685376d4d3472
Walid <ow_bouhenika@esi.dz>
2026-06-28 12:36:06 +0100

    simple 20 mins of the vid

---
+++ test1.py
@@ -0,0 +1,18 @@
+walid = 1234
+walid_string = "walid"
+
+walid_list = ["house","door","window"]
+
+walid_boolTrue = True
+walid_boolFalse = False
+
+print(walid)
+print(walid_string)
+print(walid_list)
+print(walid_boolTrue)
+print(walid_boolFalse)
+print(walid,walid_string,walid_list,walid_boolTrue,walid_boolFalse)
+ending= '\n'
+
+print("walid was here or something idk",end=ending)
+
