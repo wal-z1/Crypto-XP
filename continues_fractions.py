7e1671438fa04eea2337634404045001ca98f96b
Walid <ow_bouhenika@esi.dz>
2026-07-02 21:18:05 +0100

    cleaner

--- continues_fractions.py
+++ continues_fractions.py
@@ -1,29 +1,32 @@
 from fractions import Fraction
 import math
-def continued_fraction():
+def continued_fraction(input_numerator, input_denominator):
     """Compute the continued fraction representation of a rational number."""
     cf = []
-    print("please input your numerator and denominator")
-    input_numerator = int(input("Numerator: "))
-    input_denominator = int(input("Denominator: "))
-    x =  Fraction(input_numerator, input_denominator)
-    print("The continued fraction representation of", x, "is:")
+    x = Fraction(input_numerator, input_denominator)
     while True:
-        intger_part = x.numerator // x.denominator
-        print("integer part:", intger_part)
-        cf.append(intger_part)
-        x = 1 / (x - intger_part)
-        print("new x:", x)
+        integer_part = x.numerator // x.denominator
+        cf.append(integer_part)
+        x = 1 / (x - integer_part)
         if x.denominator == 1:
             cf.append(x.numerator)
             break
-    print(cf)
-    expr = str(cf[-1])
-    for a in reversed(cf[:-1]):
-      expr = f"{a} + 1/({expr})"
-    print(expr)
-    ## this prints like 1 + 1/(2 + 1/(3 + 1/4)) for [1,2,3,4]
-    ## the following steps : first it takes the last element of the list and then it takes the second last element and adds 1 over the last element and so on until it reaches the first element of the list.
+    return cf
 
 
-continued_fraction()
+continued_fraction(input_numerator=415, input_denominator=93)
+
+def convergentcalc(a,p_n_1=1,q_n_1=0,p_n_2=0,q_n_2=1):
+    """ calculate the convergents of a continued fraction representation."""
+    p_n = a*p_n_1 + p_n_2
+    q_n = a*q_n_1 + q_n_2
+    return p_n, q_n
+
+def convergents(cf):
+    """ calculate the convergents of a continued fraction representation."""
+    convs = []
+    if len(cf) == 1:
+        return [Fraction(cf[0], 1)]
+    else:
+        for i in cf:
+            p_n, q_n = convergentcalc()
---
+++ weiner_attack.py
@@ -0,0 +1,11 @@
+import continues_fractions
+
+p = 31
+q = 23
+n = p*q
+phi_n = (p-1)*(q-1)
+
+e = 7
+d = 283
+
+
