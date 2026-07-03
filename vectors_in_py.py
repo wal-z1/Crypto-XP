034543278ac62ee47284bab4c555feda4f513819
Walid <ow_bouhenika@esi.dz>
2026-07-03 19:12:19 +0100

    just pushing

---
+++ vectors_in_py.py
@@ -0,0 +1,15 @@
+import numpy as np
+import sympy
+
+v = np.array([1, 2, 3])
+w = np.array([4, 5, 6])
+
+M = np.array([
+  [1, 2],
+  [3, 4]]
+  )
+
+matrix = sympy.Matrix([
+  [1, 2],
+  [3, 4]]
+  )
