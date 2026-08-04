8fa4624801c05fd3627cb66ec9093a01ca3e98b2
Walid <ow_bouhenika@esi.dz>
2026-08-04 17:35:32 +0100

    orga

--- gcedx.py
+++ gcedx.py
@@ -1,4 +1,6 @@
 import sympy
-a,b ,g = sympy.gcdex(7,11)
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+a,b ,g = sympy.gcdex(x,y)
 
-print(a,b,g) 
+print(a,b,g)
