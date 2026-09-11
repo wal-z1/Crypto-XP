b03cfdbe8dfc284fd1f3d711cddbd8e8883a5f29
Walid <ow_bouhenika@esi.dz>
2026-09-11 15:39:01 +0100

    smooth criminal challnge done

--- Labs/CryptoHack/Elliptic/Encryption Chall/hardcoded_aes_decryption.py
+++ Labs/CryptoHack/Elliptic/Encryption Chall/hardcoded_aes_decryption.py
@@ -29,4 +29,3 @@ shared_secret = 1791
 iv = 'cd9da9f1c60925922377ea952afc212c'
 ciphertext = 'febcbe3a3414a730b125931dccf912d2239f3e969c4334d95ed0ec86f6449ad8'
 
-print(decrypt_flag(shared_secret, iv, ciphertext))
